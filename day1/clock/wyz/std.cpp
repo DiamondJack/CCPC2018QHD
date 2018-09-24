dc44f3d57d0f41d7c8be461eda3e1dcccea34e31
wyz <wang_yuzhong@126.com>
2018-09-24 21:10:53 +0800

    add clock vali

--- day1/clock/wyz/std.cpp
+++ day1/clock/wyz/std.cpp
@@ -7,11 +7,16 @@ int main(){
 	scanf("%d",&T);
 	while (T--){
 		int a,b;
-		scanf("%d%d",&a,&b);
+		if (scanf("%d%d",&a,&b)==EOF){
+			cerr<<"Unexpected EOF"<<endl;
+			return 0;
+		}
 		int ans=b-a;
 		if (a<=11&&b>=12) --ans;
 		if (a<=23&&b==24) --ans;
 		printf("%d\n",ans);
+		assert(0<=a&&a<b&&b<=24);
 	}
+	assert(scanf("%d",&T)==EOF);
 	return 0;
 }
