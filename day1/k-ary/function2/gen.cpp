365475345bc9ca9eeae471fcbdb97c28e06e6afd
function2-llx <function2@qq.com>
2018-09-26 16:15:36 +0800

    k-ary改数据

--- day1/k-ary/function2/gen.cpp
+++ day1/k-ary/function2/gen.cpp
@@ -8,6 +8,12 @@ int rand(int l, int r)
 	return l + rand() % len;
 }
 
+int randk()
+{
+	if (rand() % 5 == 0) return rand();
+	return rand() % 100;
+}
+
 int main()
 {
 	srand(time(NULL));
@@ -16,10 +22,10 @@ int main()
 	cout << T << endl;
 
 	int small = T - 3;
-	for (int i = 0; i < small; i++) printf("%d %d\n", rand(1, 3000), rand());
+	for (int i = 0; i < small; i++) printf("%d %d\n", rand(1, 3000), randk());
 
 	for (int i = small; i < T; i++) {
-		printf("%d %d\n", rand(40000, 60000), rand());
+		printf("%d %d\n", rand(40000, 60000), randk());
 	}
 
 	return 0;
